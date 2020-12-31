75ccc2e6682cace227dab9328e45f17e33e403c4
Sahana Ilenchezhian <sahana.chelian@gmail.com>
2020-12-30 17:22:19 -0800

    fixed changing tip feature

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var tipPercentageLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        //Get the bill amount
+        let bill = Double(billAmountTextField.text!) ?? 0
+        let tipPercentages = [0.15, 0.18, 0.20]
+        
+        //calculate tip and total
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        //update labels
+        tipPercentageLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
 }
 
